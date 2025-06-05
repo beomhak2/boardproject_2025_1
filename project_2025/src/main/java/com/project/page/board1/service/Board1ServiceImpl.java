17fa45e0416fb5d2bdc8534f367f36f6d6f6dd46
tndnjs1263 <87746813+tndnjs1263@users.noreply.github.com>
2025-06-05 18:17:43 +0900

    Update Board1ServiceImpl.java

--- project_2025/src/main/java/com/project/page/board1/service/Board1ServiceImpl.java
+++ project_2025/src/main/java/com/project/page/board1/service/Board1ServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.page.board1.dao.Board1Dao;
-import com.project.page.model.Board;
+import com.project.page.board1.model.Pager;
 import com.project.page.model.Post;
 
 @Service
@@ -15,34 +15,60 @@ public class Board1ServiceImpl implements Board1Service {
 	@Autowired
 	Board1Dao dao;
 	
+	/**
+	 * 모든 게시글 조회 및 조회수 설정
+	 */
 	@Override
-	public List<Post> list() {
-		return dao.list();
+	public List<Post> list(Pager pager) {
+		pager.setTotal(dao.selectPostCount(pager));
+		return dao.list(pager);
+	}
+	
+	/**
+	 * 페이징 숫자 가져오기
+	 */
+	@Override
+	public int getPostCount(Pager pager) {
+		return dao.selectPostCount(pager);
 	}
  
+	/**
+	 * 새 게시글 등록합니다.
+	 */
 	@Override
 	public void writePost(Post post) { 
 		dao.insertPost(post);
 	}
 
+	/**
+	 * 게시글 수정합니다.
+	 */
 	@Override
 	public int updatePost(Post post) {
 		return dao.updatePost(post);
 	}
 
+	/**
+	 * 게시글 postId 찾기
+	 */
 	@Override
 	public Post getPostById(int postId) {
 		return dao.selectPostById(postId);
 	}
 
+	/**
+	 * 게시글 삭제(del_yn = 'Y')로 변경
+	 */
 	@Override
 	public void deletePost(int postId) {
 		dao.deletePost(postId);
 	}
 
+	/**
+	 * 조회수 증가
+	 */
 	@Override
 	public void increaseViewCount(int postId) {
 		dao.increaseViewCount(postId);
-		
 	}
 }
