a60bd622cf112dd2a12dabd79559dd23a506be6a
tndnjs1263 <87746813+tndnjs1263@users.noreply.github.com>
2025-06-05 18:18:10 +0900

    Update Board1DaoOracle.java

--- project_2025/src/main/java/com/project/page/board1/dao/Board1DaoOracle.java
+++ project_2025/src/main/java/com/project/page/board1/dao/Board1DaoOracle.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.project.page.model.Board;
+import com.project.page.board1.model.Pager;
 import com.project.page.model.Post;
 
 @Repository 
@@ -15,35 +15,61 @@ public class Board1DaoOracle implements Board1Dao{
 	@Autowired
 	private SqlSession sql;
 	
+	/**
+	 * 게시글 목록 조회
+	 */
 	@Override
-	public List<Post> list() {
-		return sql.selectList("board1.selectPostList");
+	public List<Post> list(Pager pager) { 
+		List<Post> list = sql.selectList("board1.selectPostList", pager);
+		
+		return list;
+	}
+	
+	/**
+	 * 게시글 페이지
+	 */
+	@Override
+	public int selectPostCount(Pager pager) {
+		return sql.selectOne("board1.selectPostCount", pager);
 	}
 
+	/**
+	 * 게시글 입력
+	 */
 	@Override
 	public void insertPost(Post post) {
 		sql.insert("board1.insertPost", post);
 	}
 
+	/**
+	 * 게시글 수정
+	 */
 	@Override
 	public int updatePost(Post post) {
 		return sql.update("board1.updatePost", post);
 	}
 
+	/**
+	 * 게시글 아이디 찾기
+	 */
 	@Override
 	public Post selectPostById(int postId) {
 		return sql.selectOne("board1.selectPostById", postId);
 	}
 
+	/**
+	 * 게시글 삭제
+	 */
 	@Override
 	public void deletePost(int postId) {
-		sql.delete("board1.deletePost", postId);
+		sql.delete("board1.deletePostY", postId);
 	}
 
+	/**
+	 * 게시글 조회수
+	 */
 	@Override
 	public void increaseViewCount(int postId) {
 		sql.update("board1.increaseViewCount", postId);
-		
-		
 	}
 }
