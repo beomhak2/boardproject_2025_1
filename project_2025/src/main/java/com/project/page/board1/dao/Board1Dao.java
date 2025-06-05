3ecab6bc7babd0a2d26fc1fb03694f055c665e0a
tndnjs1263 <87746813+tndnjs1263@users.noreply.github.com>
2025-06-05 18:18:28 +0900

    Update Board1Dao.java

--- project_2025/src/main/java/com/project/page/board1/dao/Board1Dao.java
+++ project_2025/src/main/java/com/project/page/board1/dao/Board1Dao.java
@@ -2,23 +2,23 @@ package com.project.page.board1.dao;
 
 import java.util.List;
 
-import com.project.page.model.Board;
+import com.project.page.board1.model.Pager;
 import com.project.page.model.Post;
 
 public interface Board1Dao {
-
-	List<Post> list();
-
+	// 게시글 목록 조회
+	List<Post> list(Pager pager);
+	// 게시글 페이지
+	int selectPostCount(Pager pager);
+	// 게시글 입력
 	void insertPost(Post post);
-
+	// 게시글 수정
 	int updatePost(Post post);
-
+	// 게시글 postId 가져오기
 	Post selectPostById(int postId);
-
+	// 게시글 삭제
 	void deletePost(int postId);
-
+	// 게시글 조회수
 	void increaseViewCount(int postId);
-
-	
 }  
    
