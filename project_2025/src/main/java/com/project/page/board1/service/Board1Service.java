e6240ebeb08daaadab61775dc2d1ce4357cc34fb
tndnjs1263 <87746813+tndnjs1263@users.noreply.github.com>
2025-06-05 18:17:11 +0900

    Update Board1Service.java

--- project_2025/src/main/java/com/project/page/board1/service/Board1Service.java
+++ project_2025/src/main/java/com/project/page/board1/service/Board1Service.java
@@ -2,14 +2,14 @@ package com.project.page.board1.service;
 
 import java.util.List;
 
-import org.springframework.ui.Model;
-
-import com.project.page.model.Board;
+import com.project.page.board1.model.Pager;
 import com.project.page.model.Post;
 
 public interface Board1Service {
 	// 게시판 목록
-	List<Post> list();
+	List<Post> list(Pager pager);
+	// 게시판 페이지
+	int getPostCount(Pager pager);
 	// 게시판 작성
 	void writePost(Post post);
 	// 게시판 아이디
@@ -18,7 +18,6 @@ public interface Board1Service {
 	int updatePost(Post post);
 	// 게시판 삭제
 	void deletePost(int postId);
-	//죄회수 증가
+	// 죄회수 증가
 	void increaseViewCount(int postId);
- 
 }
